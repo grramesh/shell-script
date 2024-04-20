fac2d1452975411ab0e62124c030cbf4acb4b7cd
Ramesh <ramgr498@gmail.com>
2024-04-20 06:57:02 +0530

    conversation b/w two person

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+**Conversation between two persons**
+
+echo "ramesh:: hii suresh how are you ?"
+echo "suresh:: iam good how are you ramesh"
+echo "ramesh:: i need some info from you"
+echo "suresh :: please let me know ramesh"
--- read.md
+++
@@ -1,7 +0,0 @@
-#Shell script
-**Learning Shell Script**
-
-hello planet,
-iam learning devops
-steps
-signup to git hub
