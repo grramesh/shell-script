47e3617993c0f9f9be13e0658e6046995b2ccc5c
Ramesh <ramgr498@gmail.com>
2024-04-20 06:44:04 +0530

    first commit

---
+++ 01-hello world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+**step** #writting steps
+echo"Hello iam learning devops with AWS"
+step 1: sign up git hub
+step 2: clone it with local folder
+step 3: create git hub token 
+step 4: git add . ; git commit -m "enter related message" ;git push -u origin main
+step 5: git pull
