9d374c120ee9d869cf9f9056bce9d98ea68c3c1f
Ramesh <ramgr498@gmail.com>
2024-04-20 10:26:04 +0530

    sum of two numbers

---
+++ 07-data-types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Declaring variables
+
+NO1=$1 #Givien arguments $1 and $2 hence no need to come and enter values in hot code
+NO2=$2 #addition of two numbers
+
+#referring variables
+
+SUM=$(($NO1+$NO2))
