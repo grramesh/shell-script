de22b76214df434d0263e8ca012e7ac329fdf841
Ramesh <ramgr498@gmail.com>
2024-04-20 07:14:15 +0530

    conversation between two persons 3-variables folder

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+**Conversation between two persons**
+
+person1=$1
+person2=$2
+
+echo "$person1:: hii $person2 how are you ?"
+echo "$person2:: iam good how are you $person1"
+echo "$person1:: i need some info from you"
+echo "$person2 :: please let me know $person1"
