df4ec9d2d1d985188216af71db8d39fdacfd9301
Ramesh <ramgr498@gmail.com>
2024-04-20 10:29:42 +0530

    sum of two numbers

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -5,6 +5,7 @@
 NO1=$1 #Givien arguments $1 and $2 hence no need to come and enter values in hot code
 NO2=$2 #addition of two numbers
 
-#referring variables
-
 SUM=$(($NO1+$NO2))
+
+
+echo "Total of $NO1 and $NO2 is : $SUM"
