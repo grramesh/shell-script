efe742f26694589f018ff1b3fef025a7f1d27f6d
Ramesh <ramgr498@gmail.com>
2024-04-20 08:59:54 +0530

    array

--- 06-variables.sh
+++ 06-variables.sh
@@ -5,6 +5,7 @@ movies=("inception" "pushpa" "pokiri")
 # list always start from zero
 # size of above array is 3
 # index are 0,1,2
+# list all is ---> @ 
 
 echo "First movie is : ${movies[0]}"
-echo "List of all movies: ${all movies[@]}"  # list all is ---> @    
+echo "List of all movies: ${all movies[@]}"     
