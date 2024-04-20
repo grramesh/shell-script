072e9b8269b435b0f9848371faaf61d9cef7714e
Ramesh <ramgr498@gmail.com>
2024-04-20 19:23:07 +0530

    conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# to check whether a number is -ge than 20 or not 
+$NUMBER = 5
+if ($NUMBER -gt 10)
+   then
+echo "Given number $NUMBER is > 10"
+  else "given number $NUMBER is <10"
+  fi
