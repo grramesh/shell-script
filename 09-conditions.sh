3315d30f329bbbe87127b3d34e73829c32a15d4c
Ramesh <ramgr498@gmail.com>
2024-04-20 19:45:42 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # to check whether a number is -ge than  or not 
-NUMBER=$1
+NUMBER=$1   
 if [ $NUMBER -gt 10 ] #Space between brackets should be there
    then
        echo "Given number $NUMBER is > 10"
   else 
        echo "given number $NUMBER is  < 10"
+  fi
+
+
+  NUMBER=$1   
+if [ $NUMBER -lt 25 ] #Space between brackets should be there
+   then
+       echo "Given number $NUMBER is < 25"
+  else 
+       echo "given number $NUMBER is  > 25"
   fi
