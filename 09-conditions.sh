4ba02b749070deae3dd9bbfc1517644d37b6144f
Ramesh <ramgr498@gmail.com>
2024-04-20 19:46:43 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -10,10 +10,10 @@ if [ $NUMBER -gt 10 ] #Space between brackets should be there
   fi
 
 
-  NUMBER=$1   
+  NUMBER=$2   
 if [ $NUMBER -lt 25 ] #Space between brackets should be there
    then
        echo "Given number $NUMBER is < 25"
   else 
        echo "given number $NUMBER is  > 25"
-  fi
+  
