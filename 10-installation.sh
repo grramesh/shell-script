fc25ea5ecb70dd55a9d643c04cb7b6bb4467bf0d
Ramesh <ramgr498@gmail.com>
2024-04-20 20:47:54 +0530

    My sql installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #we can use(Command --> id ) to know normal(1-127 is normal user) or super user(0 is super user) or (command--> id -u) 
 # How do you run a command inside shell script and take the output. (command--> variable name=$( ) it run and execute the command and give it to variable name)
-# command--> USERID=$(id -u) here USERID is variable name. 
+# command--> USERID=$(id -u) here USERID is variable name.
+#when we are installing we should be in super user. 
 
 USERID=$(id -u)
 
-if [ $USER -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "please run this script in root access"
 else
@@ -13,4 +14,4 @@ echo "you are root user."
 
 fi
 
-dnf install mysql -y #when we are installing we should be in super user.
+dnf install mysql -y 
