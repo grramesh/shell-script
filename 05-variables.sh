d69bf3181220e6ec70766c8ccdb69a247df6200b
Ramesh <ramgr498@gmail.com>
2024-04-20 08:20:42 +0530

    credentials

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter your username::"
+read username #here user name is variables
+
+echo "please enter the password"
+read password
+
+echo "username is : $username, password is $password"
+
