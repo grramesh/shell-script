5a034333ef75a11eb7813d6442d4a1da72fd49f4
Ramesh <ramgr498@gmail.com>
2024-04-22 13:38:26 +0530

    loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -2,7 +2,7 @@
 
 #Loops---> to install 20 packages using funtions is difficult/time taking process hence we are using loops
 
-for i in {i..20}   #it will loop from 1 to 20
+for i in {1..20}
 do
-   echo $ i
+   echo $i
 done
