f587ed91281a3351040895823ee19307f9a7a93c
Ramesh <ramgr498@gmail.com>
2024-04-22 13:19:15 +0530

    loops

--- 12-colours.sh
+++ 12-colours.sh
@@ -3,6 +3,8 @@
 #colors command---> echo -e "e\[31m enter text "  30--> black 31-->RED -->32 GREEN --->33 YELLOW----> 34 blue-->35 PURPLE
 # also we can split colour b/w two variables--> echo -e "e\[32m text e\[34m text" --->  e\[0m text FOR WHITE COLOUR
 
+# To identify mistake--- view logs---> cd /tmp ---> ls -la ----> cat <file-name> ---> cd
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
---
+++ 13-loops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Loops---> to install 20 packages using funtions is difficult/time taking process hence we are using loops
+
+for i in {i..20}   #it will loop from 1 to 20
+do
+   echo $i
+done
