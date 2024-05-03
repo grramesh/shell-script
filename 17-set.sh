3ae5eeda8c466e96164622bbc95e8a564224b5d0
Ramesh <ramgr498@gmail.com>
2024-05-03 16:53:39 +0530

    source code

---
+++ 17-set.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e 
+# set -e give on top ,then shell script checking manually for errors
+# for good user experiance we are using validates if not 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script in root access"
+    exit 1 # manually exit if error come.# 
+else
+echo "you are root user."
+fi
+
+dnf install mysqlsdd -y
+dnf install git -y 
+
+echo "is still script proceeding"
