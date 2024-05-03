863a817d595f172dba6fb0f962a8a293b29fc14e
Ramesh <ramgr498@gmail.com>
2024-05-03 11:13:46 +0530

    calling other script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -3,5 +3,7 @@ COURSE="Devops from current script"
 echo "Before calling other script course:$COURSE"
 echo "Process ID of current shell script $$"
 
-Source./16-other-script.sh
+# ./16-other-script.sh #1st way after calling otherscript, PID are differnt and changes happened ohter script will not reflect in current script
+source./16-other-script.sh #2nd way after calling other script, PID are same and changes happened ohter script will reflect in current script
 echo "after calling other script course:$COURSE"
+
