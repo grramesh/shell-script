25c5fb0c33cee0d2444240d2a4e9de44fa69ddf7
Ramesh <ramgr498@gmail.com>
2024-05-03 11:15:04 +0530

    calling other script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -4,6 +4,6 @@ echo "Before calling other script course:$COURSE"
 echo "Process ID of current shell script $$"
 
 # ./16-other-script.sh #1st way after calling otherscript, PID are differnt and changes happened ohter script will not reflect in current script
-source./16-other-script.sh #2nd way after calling other script, PID are same and changes happened ohter script will reflect in current script
+source ./16-other-script.sh #2nd way after calling other script, PID are same and changes happened ohter script will reflect in current script
 echo "after calling other script course:$COURSE"
 
